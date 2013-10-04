580263d9559f4a411127bcea11352507e6292523
Brice <brice.picaud@sage.com>
2013-10-04 15:56:02 +0200

    new specs

---
+++ spec/requests/layout_links_spec.rb
@@ -0,0 +1,24 @@
+require 'spec_helper'
+  describe "LayoutLinks" do
+
+    it "should find homepage at /" do
+      get '/'
+      response.should have_selector('title', :content => "Accueil")
+    end
+    
+    it "should find about page at /about" do
+      get '/about'
+      response.should have_selector('title', :content => "About")
+    end
+
+    it "should find contact page at /contact" do
+      get '/contact'
+      response.should have_selector('title', :content => "Contact")
+    end
+
+    it "should find  help page at /help" do
+      get '/help'
+      response.should have_selector('title', :content => "Help")
+    end
+    
+end
